d5eed2a960e42a693fc3170996d353800b73e047
hamzaV2000 <70440394+hamzaV2000@users.noreply.github.com>
2023-01-09 17:58:51 +0300

    deleted ip thread.

--- src/main/java/com/example/controller/Utility.java
+++ src/main/java/com/example/controller/Utility.java
@@ -9,6 +9,7 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
 import java.net.URL;
 import java.security.Principal;
 
@@ -18,27 +19,24 @@ public class Utility {
 
     static String IP_ADDRESS;
     private Utility (){
-        new Thread(() -> {
-            while (true){
-                try {
-                    URL whatismyip = new URL("https://api.ipify.org?format=json");
-                    BufferedReader in = null;
-                    in = new BufferedReader(new InputStreamReader(
-                            whatismyip.openStream()));
-                    IP_ADDRESS = in.readLine(); //you get the IP as a String
-                    System.out.println(IP_ADDRESS);
-                } catch (IOException e) {
-                    throw new RuntimeException(e);
-                }
-                JSONObject jsonObject = new JSONObject(IP_ADDRESS);
-                IP_ADDRESS = "http://" + jsonObject.getString("ip") + "/python";
-                try {
-                    Thread.sleep(1000 * 300);
-                } catch (InterruptedException e) {
-                    throw new RuntimeException(e);
-                }
-            }
-        }).start();
+
+        URL whatismyip = null;
+        try {
+            whatismyip = new URL("https://api.ipify.org?format=json");
+            BufferedReader in = null;
+            in = new BufferedReader(new InputStreamReader(
+                    whatismyip.openStream()));
+            IP_ADDRESS = in.readLine(); //you get the IP as a String
+            System.out.println(IP_ADDRESS);
+            JSONObject jsonObject = new JSONObject(IP_ADDRESS);
+            IP_ADDRESS = "http://" + jsonObject.getString("ip") + "/python";
+        } catch (MalformedURLException e) {
+            throw new RuntimeException(e);
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+
+
     }
     static User getUser(Principal principal, UserService userService) {
         String name = principal.getName();
